f1695ed7b6d5a37293b7d53a3949f082225a17b6
Zoran Zaric <zz@zoranzaric.de>
2023-10-03 19:33:20 +0200

    Add arp command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{net::IpAddr, time::Duration};
 use clap::{Parser, Subcommand};
 use color_eyre::Result;
 use colored::Colorize;
+use ipnet::Ipv4Net;
 
 #[derive(Debug, Clone, Parser)]
 #[command(name = "rdiag", author, about, long_about = None)]
@@ -14,9 +15,16 @@ pub struct Opts {
 
 #[derive(Debug, Clone, Subcommand)]
 pub enum Command {
-    Ping { addresses: Vec<IpAddr>, 
+    Ping {
+        addresses: Vec<IpAddr>,
         #[arg(long, default_value = "1000")]
-        limit : u64 },
+        limit: u64,
+    },
+    Arp {
+        net: Ipv4Net,
+        #[arg(default_value = "2")]
+        iterations: u32,
+    },
 }
 
 fn main() -> Result<()> {
@@ -48,7 +56,64 @@ fn main() -> Result<()> {
                 }
             }
         }
+        Command::Arp { net, iterations } => {
+            arp::run(&net, iterations);
+        }
     }
 
     Ok(())
+}
+
+mod arp {
+    use std::{collections::HashMap, net::Ipv4Addr, time::Duration};
+
+    use ipnet::Ipv4Net;
+    use libarp::{client::ArpClient, interfaces::MacAddr};
+
+    pub fn run(net: &Ipv4Net, iterations: u32) {
+        eprintln!("Scanning {} for {} passes", net, iterations);
+        let mut arp_client = ArpClient::new().expect("Could not get ArpClient");
+        let mut l = Vec::new();
+
+        for i in 1..=iterations {
+            eprintln!("Pass {i}...");
+            for addr in net.hosts() {
+                let res = query(&mut arp_client, addr, Some(Duration::from_millis(100)));
+                if let Ok((_, mac_addr)) = res {
+                    let ip = format!("{addr}");
+                    let mac = format!("{mac_addr}");
+                    l.push((ip, mac));
+                }
+            }
+            eprintln!("Pass {i} done");
+        }
+
+        let mut m: HashMap<String, HashMap<String, u32>> = HashMap::new();
+        for (ip, mac) in l {
+            m.entry(ip)
+                .and_modify(|n| {
+                    n.entry(mac.clone()).and_modify(|x| *x += 1).or_insert(1);
+                })
+                .or_insert({
+                    let mut x = HashMap::new();
+                    x.insert(mac, 1);
+                    x
+                });
+        }
+
+        for (ip, x) in m {
+            if x.len() > 1 {
+                println!("{ip}: {x:?}");
+            }
+        }
+    }
+
+    fn query(
+        arp_client: &mut ArpClient,
+        addr: Ipv4Addr,
+        timeout: Option<Duration>,
+    ) -> Result<(Ipv4Addr, MacAddr), std::io::Error> {
+        let mac = arp_client.ip_to_mac(addr, timeout)?;
+        Ok((addr, mac))
+    }
 }
